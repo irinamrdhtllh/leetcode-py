43e7d940b27271df2ac6be6a1caad456bf41137a
Irina M. S <irinamardhatillah@gmail.com>
2024-11-06 09:59:38 +0700

    Solve valid anagram problem

---
+++ test/test_valid_anagram.py
@@ -0,0 +1,23 @@
+from src.valid_anagram import isAnagram
+
+
+def test_case_1():
+    s = "anagram"
+    t = "nagaram"
+    actual = isAnagram(s, t)
+    expected = True
+    assert actual == expected
+
+def test_case_2():
+    s = "rat"
+    t = "car"
+    actual = isAnagram(s, t)
+    expected = False
+    assert actual == expected
+
+def test_case_3():
+    s = "aacc"
+    t = "ccac"
+    actual = isAnagram(s, t)
+    expected = False
+    assert actual == expected
